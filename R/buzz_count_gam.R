23c03fa8f1f8836852d7e9c1c362a835427c8429
Edward Hurme <edwardhurme@gmail.com>
2022-05-12 17:57:07 +0200

    setting up buzz counts

---
+++ R/buzz_count_gam.R
@@ -0,0 +1,24 @@
+# Buzz count gam
+
+## load libraries
+library(pacman)
+p_load(data.table, lubridate, tidyverse, ggplot2,
+       mgcv, suncalc, janitor, corrplot)
+
+bdf <- fread("../../../Dropbox/MPI/BatsandGrass/Data/buzzes-melina-meadows_MD.csv") %>% clean_names()
+tmpsplt <- bdf$buzzfile_na_means_no_buzz %>% strsplit(., split = "-")
+dates <- sapply(tmpsplt, "[", 1)
+loc_hours <- sapply(tmpsplt, "[", 2)
+loc <- sapply(strsplit(loc_hours, "_"), "[", 1)
+hours <- sapply(strsplit(loc_hours, "_"), "[", 2)
+
+bdf$timestamp <- ymd_hms(paste(dates, hours))
+bdf$time %>% mdy
+bdf %>% summary
+
+## summarize per sampling minute?
+
+# Sampling hours?
+
+
+
